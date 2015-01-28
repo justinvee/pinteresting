bb5c7dd2face249f67c329d0211817b2838eb6fa
Justin Vekinis <justinvekinis@gmail.com>
2015-01-28 13:17:41 -0500

    Fixed a bug where Edit/Del links show up on pins without users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-	has_many :pins
+	has_many :pins, dependent: :destroy # When a user deletes account, pins will be deleted.
 
 	validates :name, presence: true
 end
